edd4932fec8f3f648231e5bb6c53c2d63ed479a7
lplnt <phd@live.ru>
2022-11-28 21:33:28 +0300

    third commit. находим элемент в массиве[200~{

--- Program.cs
+++ Program.cs
@@ -6,3 +6,14 @@
 string[] array1 = new string[] {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science"};;
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIf(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
